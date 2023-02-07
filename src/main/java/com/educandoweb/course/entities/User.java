9d470feaa94f174164f32c37ed30f9ddd95c4ae6
Iarley Melo <iarleym771@gmail.com>
2023-02-07 16:54:32 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,24 +3,33 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
-	private String fone;
+	private String phone;
 	private String passWord;
 
 	public User() {
 	}
 
-	public User(Long id, String name, String email, String fone, String passWord) {
+	public User(Long id, String name, String email, String phone, String passWord) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
-		this.fone = fone;
+		this.phone = phone;
 		this.passWord = passWord;
 	}
 
@@ -48,12 +57,12 @@ public class User implements Serializable{
 		this.email = email;
 	}
 
-	public String getFone() {
-		return fone;
+	public String getPhone() {
+		return phone;
 	}
 
-	public void setFone(String fone) {
-		this.fone = fone;
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
 	public String getPassWord() {
